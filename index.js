5505114a7b01bdf15025788abd55a763e3a22866
Mansoor Sahito <mansoorsahito601@gmail.com>
2025-11-14 15:47:49 +0500

    Nodemon and Routes Add

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+// console.log("Hello World")
+const express = require("express")
+
+const app = express()
+
+const port = 3000;
+
+app.get("/",(req,res)=>{
+    res.send("Hello World for Backend Changed")
+})
+
+app.get("/about",(req,res)=>{
+    res.send("About Page")
+})
+
+
+/// 
+app.get("/home",(req,res)=>{
+    res.send("Home Page")
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on: ${port}`)
+})

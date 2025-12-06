4ee8642dd65823c1d44c9e1f762a8581bd974d75
Mansoor Ali <mansoorsahito601@gmail.com>
2025-12-06 11:21:03 +0500

    Book Post Successfully

--- index.js
+++ index.js
@@ -1,57 +1,40 @@
-// const express = require("express")
-
-// const mongoose = require("mongoose")
-// const app = express()
-
-// require("dotenv").config()
-
-// const port = 3000
-
-// app.get("/Home",(req,res)=>{
-//     res.send("App Listing on Home Page")
-// })
-
-// app.get("/projects",(req,res)=>{
-//     res.send("App Listing on Projects Page")
-// })
-
-// app.listen(port,()=>{
-//     console.log("App listing on Port 3000")
-// })
-
-
-// // const connnectionString = process.env.CONNECTION_STRING
-// const databaseURl = process.env.CONNECTION_STRING
-
-// // mongoose.connect(connnectionString)
-// // .then(()=>console.log("Database Connnected"))
-// // .catch((error)=>console.log(error))
-
-// mongoose.connect(databaseURl)
-// .then(()=>console.log("DataBase Connected Successfully"))
-// .catch((error)=>console.log(error))
-
 
 console.log("hello This is WOrking ")
 
 const express = require("express")
 
+require("dotenv").config()
+
+const mongoose = require("mongoose")
 const app = express()
 
 
-app.listen(3000,()=>{
+const port = 3000
+
+app.use(express.json())
+//// Books Schema
+
+const booKSchema =  new mongoose.Schema({})
+
+const BookModel = mongoose.model("Book", booKSchema)
+
+
+// Book Create Route
+
+app.post("/books",async(req,res)=>{
+    const newbook = await BookModel.create(req.body)
+    res.status(201).json(newbook)
+})
+
+
+
+const databaseconnection = process.env.CONNECTION_STRING
+
+
+mongoose.connect(databaseconnection)
+.then(()=>console.log("Database Connected Successfully"))
+.catch((error)=>console.log(error))
+
+app.listen(port,()=>{
     console.log("App is listing on Port 3000")
-})
-
-
-app.get("/home", (req,res)=>{
-    res.send("App is listing on home Page")
-})
-
-app.get("/likes",(req,res)=>{
-    res.send("App is Running on Likes Page")
-})
-
-app.get("/projects",(req,res)=>{
-    res.send("Projects page is listing ")
 })

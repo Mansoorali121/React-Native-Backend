ca23dd103054713f0cee5ef0528847962155897b
Mansoor Ali <mansoorsahito601@gmail.com>
2025-12-10 11:06:53 +0500

    Josn Body Parser Add

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-// console.log("hello This is WOrking ");
 
-// const express = require("express");
-
-// require("dotenv").config();
-
-// const mongoose = require("mongoose");
-// const app = express();
-
-// const port = 3000;
 
 // // app.use(express.json());
 // // //// Books Schema
 
 
 
-// const databaseconnection = process.env.CONNECTION_STRING;
 
-// mongoose
-//   .connect(databaseconnection)
-//   .then(() => console.log("Mongo Db Database Connected Successfully: "))
-//   .catch((error) => console.log(error));
-
-// app.listen(port, () => {
-//   console.log(`App is Listing on Provided Port ${port}`);
-// });
 
 
 const express = require("express")
@@ -36,15 +18,26 @@ const mongoose = require("mongoose")
 
 const port = 3000
 
+// JSON Body Parser 
+app.use(express.json)
+
+// Creatinng Database
+
+
+
+const StudentsNames = mongoose.Schema({})
+
+StudentsModel = mongoose.model("StdNames",StudentsNames)
+
 
 app.listen(port,()=>{
   console.log("App is Listing on Port 3000")
 })
 
 
-app.use("/home",(req,res)=>{
-  res.send("Home Page is Listing")
-})
+// app.use("/home",(req,res)=>{
+//   res.send("Home Page is Listing")
+// })
 
 
 const databaseconnection = process.env.CONNECTION_STRING

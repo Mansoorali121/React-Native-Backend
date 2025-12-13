791363911c8b18c301c8cbdeaaaab8ff50900952
Mansoor Ali <mansoorsahito601@gmail.com>
2025-12-13 10:40:09 +0500

    Localization

--- vallidators/Studentvalidator.js
+++ vallidators/Studentvalidator.js
@@ -1,48 +1,5 @@
-// // Adding Express Validator Library
-// const { body, validationResult, param } = require("express-validator");
+//// Localization ////
 
-// const createStudentValidation = [
-//   body("StudentName")
-//     .notEmpty()
-//     .withMessage("Student Name is Required")
-//     // .isLength({ min: 5, max: 12 })
-//     .withMessage("Class should be between to 12 "),
-//   body("Age")
-//     .notEmpty()
-//     .withMessage("Student Age is required")
-//     // .isInt({ min: 18, max: 30 })
-//     .withMessage("Student Age must be between 18 and 30"),
-
-//   body("Class")
-//     .notEmpty()
-//     .withMessage("Class can not be Empty ")
-//     // .isLength({ min: 5, max: 12 })
-//     .withMessage("Class should be between to 12 "),
-//   body("Image").notEmpty().isURL().withMessage("Image Must be a valid URL"),
-// ];
-
-// const StudentUpdatevalidation = [
-//   param("id").isMongoId("").withMessage("Invalid Book Id"),
-
-//   body("StudentName")
-//     .optional()
-//     .withMessage("Student Name is Optional While updation")
-//     .isLength({ min: 5, max: 15 })
-//     .withMessage("Class should be between 5 to 12 "),
-// ];
-
-// const handlevalidationErrors = (req,res,next) => {
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return res.status(400).json({ error: errors.array() });
-//   }
-//   next()
-// };
-// module.exports = {
-//     createStudentValidation,
-//     StudentUpdatevalidation,
-//     handlevalidationErrors
-// }
 
 const { body, validationResult, param } = require("express-validator");
 
